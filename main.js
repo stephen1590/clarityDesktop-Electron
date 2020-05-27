665c40c18528c35dc7337bd8811215699c1e9bd5
stephen1590 <54494451+stephen1590@users.noreply.github.com>
2020-05-27 08:54:45 -0500

    resolved a dependency

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-const prompt = require('electron-prompt');
 
 const { app, BrowserWindow } = electron;
 
